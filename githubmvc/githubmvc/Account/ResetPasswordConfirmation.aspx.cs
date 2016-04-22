9b3d96aa9bb3f87e62275a74cfeabfcd7c7a3262
rohinaudi <rokum@microsoft.com>
2016-04-22 18:33:14 +0530

    files added
    new files added dude

---
+++ githubmvc/githubmvc/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace githubmvc.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ githubmvc/githubmvc/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(githubmvc.Startup))]
+namespace githubmvc
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
